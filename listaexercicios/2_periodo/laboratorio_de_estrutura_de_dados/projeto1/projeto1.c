c468b7ea5f74922b1b3da3d67ec0053e8e2a722e
Jefferson Michael <jefferson_michael407@hotmail.com>
2018-09-08 15:37:48 -0300

    falha de segmentação

--- listaexercicios/2_periodo/laboratorio_de_estrutura_de_dados/projeto1/projeto1.c
+++ listaexercicios/2_periodo/laboratorio_de_estrutura_de_dados/projeto1/projeto1.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sting.h>
+#include <string.h>
 #include <time.h>
 
-srand(time(NULL));
 
 typedef struct placar{
 	int tent,resp1,resp2,*numeros;
@@ -12,17 +11,21 @@ typedef struct placar{
 void ler_senha(int *aposta,int n){
 	int i,j,x=0;
 	for(i = 0; i<n; i++){
+		//garante que o valor digitado esta de 1 a 9
 		while(x <= 0 || x > 9){
 			scanf("%i",&x);
 		}
+		//verifica se o usuario ja dijitou o numero antes
 		for(j = 0; j < i; j++){
-			if(aposta[i] == aposta[j]){
+			if(x == aposta[j]){
 				x = 0;
 				i--;
+				break;
 			}
 		}
+		//se o x nao for igual a zero ele faz a atribuiçao
 		if(x) aposta[i] = x;
-		x = 0
+		x = 0;
 	}
 }
 
@@ -30,9 +33,10 @@ void sorteia_valores(int *sorteio, int n){
 	char rule = 0;
 	int i,j;
 	for(i = 0; i < n;i++){
+		//sorteia um valor de 0 a 8 e soma 1
 		sorteio[i] = (rand() % 9) + 1;
-		for(j = 0;j<n;j++){
-			if(j == i) continue;
+		//verifica se o numero ja foi usado antes
+		for(j = 0;j<i;j++){
 			if(sorteio[i] == sorteio[j]){
 				i--;
 				break;
@@ -45,8 +49,9 @@ void compara_senha (int *senha, int *senhaus, int *resp1, int *resp2,int n){
 	int i,j;
 	*resp1 = 0;
 	*resp2 = 0;
-	for(i = 0; i<n; i++){
-		for(j = 0; j < n;j++){
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++){
+			if(i == j) continue;
 			if(senha[i] == senhaus[j]) *resp2++;
 		}
 	}
@@ -60,8 +65,22 @@ void compara_senha (int *senha, int *senhaus, int *resp1, int *resp2,int n){
 
 
 int main(){
-	int *a,*b,n = 5;
+	srand(time(NULL));
+	int *a,*b,*d,*e,n = 5;
 	a = (int*)malloc(sizeof(int) * n);
 	b = (int*)malloc(sizeof(int) * n);
-	
+	ler_senha(a,n);
+	for(int i = 0; i < n; i ++){
+		printf("%i ",*(a+i));
+	}
+	printf("\n");
+
+	sorteia_valores(b,n);
+	for(int i = 0; i < n; i ++){
+		printf("%i ",*(b+i));
+	}
+	printf("\n");
+
+	compara_senha(b,a,d,e,n);
+	printf("%i %i\n",*d,*e);
 }
