5daf5e53117589f6c9a514c3120cad2642aa1932
Jefferson Michael <jefferson_michael407@hotmail.com>
2018-09-28 10:41:48 -0300

    lista de compras

--- listaexercicios/2_periodo/monitoria/Manipulando Lista.c
+++ listaexercicios/2_periodo/monitoria/Manipulando Lista.c
@@ -38,7 +38,7 @@ void insereini(int a,lista **list){
 //remove o primeiro elemento que seja igual a chave
 void terminate(lista **list,int a){
 	if(*list == NULL){
-		printf("Nao encontrado\n");
+		printf("Nao encontrado.");
 		return;
 	}
 	if((*list) -> chave == a){
@@ -46,7 +46,7 @@ void terminate(lista **list,int a){
 		aux = (*list) -> prox;
 		free(*list);
 		*list = aux;
-		printf("%i TERMINATED.\n",a);
+		printf("%i Eliminado.",a);
 		return;
 	}
 	terminate(&(*list) -> prox,a);
@@ -56,36 +56,60 @@ void printa(lista *list){
 	if(list == NULL) return;
 	printf("%i ",list -> chave);
 	if(list -> prox == NULL){
-		printf("\n");
 		return;
 	}
 	printa(list -> prox);
 }
 
+void busca(lista *list,int a){
+	if(list == NULL){
+		printf("Nao encontrado!");
+		return;
+	}
+	if(list -> chave == a){
+		printf("Elemento %i encontrado na lista.",a);
+		return;
+	}
+	busca(list -> prox,a);
+}
+
 int main(){
 	lista *list = NULL;
 	int a,n=0;
-	while(n < 5){
-		printf("1 fim 2 começo 3 printa 4 terminate 6 fim:");
+	system("clear");
+	while(n < 6){
+		printf("\n\n1 adiciona no fim.\n2 adiciona no começo.\n3 printa lista.\n4 deletar elemento.\n5 busca.\n6 encerra program.\nDigite sua opcao:");
 		scanf("%i",&n);
 		if(n == 1){
-			printf("Digite a chave:");
+			printf("Digite a chave para adicionar no fim:");
 			scanf("%i",&a);
 			alocafim(a,&list);
+			system("clear");
 		}
 		if(n == 2){
-			printf("Digite a chave:");
+			printf("Digite a chave para adicionar no comeco:");
 			scanf("%i",&a);
 			insereini(a,&list);
+			system("clear");
 		}
 		if(n == 3){
+			system("clear");
+			printf("lista: ");
 			printa(list);
 		}
 		if(n == 4){
 			printf("Digite a chave a ser removida:");
 			scanf("%i",&a);
+			system("clear");
 			terminate(&list,a);
 		}
+		if(n == 5){
+			printf("Digite a chave que deseja buscar:");
+			scanf("%i",&a);
+			system("clear");
+			busca(list,a);
+		}
+		setbuf(stdin,NULL);
 	}
 
 
