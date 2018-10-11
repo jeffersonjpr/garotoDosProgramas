a95dc1f2ade2e4b19f601a5eb20f1bae480a1b96
Jefferson Michael <jefferson_michael407@hotmail.com>
2018-10-11 17:52:01 -0300

    multiuso

--- listaexercicios/2_periodo/random/listacircular.c
+++ listaexercicios/2_periodo/random/listacircular.c
@@ -16,25 +16,49 @@ No * alocano(int n){
 	return aux;
 }
 //insere inicio
-No * insereini(No *no,int n){
-	if(no == NULL){
-		no = vazia(no,n);
-		if(no == NULL) return NULL;
-		no -> prox = no;
-		return no;
+No * insereini(No **no,int n){
+	if(*no == NULL){
+		*no = alocano(n);
+		if(*no == NULL) return NULL;
+		(*no) -> prox = *no;
+		return *no;
 	}
+	No *aux;
+	aux = alocano(n);
+	if (aux == NULL) return *no;
+	aux -> prox = (*no) -> prox;
+	(*no) -> prox = aux;
+	*no = aux;
+	return *no;
 }
 
-//insere fim
-No * insereini(No *no,int n){
-	if(no == NULL){
-		no = alocano(n);
-		if(no == NULL) return NULL;
-		no -> prox = no;
-		return no;
+void printar(No *no,No *aux){
+	if(no == aux){
+		printf("%i\n",aux -> chave);
+		return;
 	}
+	printf("%i ",aux -> chave);
+	printar(no,aux -> prox);
 }
 int main(){
-	No *no;
-
+	No *no = NULL;
+	int n,x;
+	while(1 == 1){
+		printf("1 inicio\n");
+		printf("2 fim\n");
+		printf("3 printar\n");
+		printf("Escolha opcao:");
+		scanf("%i",&x);
+		if(x > 10) break;
+		if(x == 1){
+			printf("Digite a chave:");
+			scanf("%i",&n);
+			no = insereini(&no,n);
+			continue;
+		}
+		if(x == 3){
+			printar(no,no->prox);
+			continue;
+		}
+	}
 }
