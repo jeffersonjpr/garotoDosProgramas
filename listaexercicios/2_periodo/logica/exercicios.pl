ecc543b70e0b634915be29f293708314cac2bf65
jeffersonjpr <jefferson_michael407@hotmail.com>
2018-12-02 20:11:39 -0200

    02122018

--- listaexercicios/2_periodo/logica/exercicios.pl
+++ listaexercicios/2_periodo/logica/exercicios.pl
@@ -29,3 +29,24 @@ fatorial(N,F):-
     Nx is N - 1,
     fatorial(Nx,Fx),
     F is N * Fx.
+
+%exercicio5: verifica se é primo
+eprimo(X,Y):-
+    X=:=Y,
+    !.
+eprimo(X,Y):-
+    X mod Y =\= 0,
+    T is Y + 1,
+    eprimo(X,T).
+primo(N):-
+    eprimo(N,2).
+
+%exercicio7:
+medida(cozinha, 2.0, 3.0).
+medida(sala, 4.0, 5.5).
+medida(quarto, 3.0, 3.5).
+medida(banheiro,1.5, 2.0).
+
+tamanho(Comodo,Area):-
+    medida(Comodo,X,Y),
+    Area is X * Y.
