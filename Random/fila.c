2303c1e20c4e0bbf0bcbe0b77498cb9d92088402
jeffersonjp <jefferson_michael407@hotmail.com>
2019-03-13 19:36:29 -0300

    relembrando

---
+++ Random/fila.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct fila{
+        int id;
+        struct fila *prox;
+}fila;
+
+
+
+int insere (int id, fila *aux){
+        if(aux == NULL){
+
+        }
+}
